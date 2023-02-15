7579a8c217d96cf35a2609d8621c9bcc50a9a545
UnitedPuggs <edpoulsonv@hotmail.com>
2023-02-14 17:06:09 -0800

    wip: styling changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,8 +12,8 @@
         <a href="/projects" class="nav-btn">Projects</a>
     </div>
     <div id="home" class="flex flex-col justify-center items-center">
-        <img src="/assets/rocket.jpg" alt="" width="890" class="mt-2 h-[30rem]">
-        <div class="bg-white px-24 pb-8 drop-shadow-2xl max-w-4xl rounded-sm">
+        <img src="/assets/rocket.jpg" alt="" width="896" class="mt-2 h-[30rem] rounded-t-md">
+        <div class="bg-white px-24 pb-8 drop-shadow-2xl max-w-4xl rounded-b-md">
             <p class="mt-2 font-sans font-normal">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
             Pellentesque tempor orci ac malesuada faucibus. Cras at varius diam. 
@@ -22,6 +22,21 @@
             Duis non massa eleifend tortor mattis euismod. Mauris consectetur ligula et tellus convallis, at malesuada erat sollicitudin. 
             Maecenas ut pharetra ante, eu iaculis massa.
             Aliquam et hendrerit magna, eget fermentum dolor. Donec placerat porta mi, vel auctor mi malesuada sed. Nullam euismod cursus ante, vitae venenatis odio vehicula in. Cras dignissim, ante a congue sagittis, nulla nunc laoreet lorem, sed dapibus lorem ante sit amet felis. Aenean a lorem vel velit lobortis sollicitudin. Vivamus luctus convallis aliquet. Praesent egestas massa sit amet tellus commodo, quis aliquet velit tempus. Mauris ac enim id urna sollicitudin semper varius id leo. Donec condimentum ligula sit amet velit interdum, id eleifend lectus feugiat.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec facilisis justo odio. Pellentesque gravida ornare nisi at tristique. Mauris erat arcu, vulputate ut tristique non, posuere feugiat felis. Curabitur mattis lacinia ante, at posuere purus rutrum ut. Cras in lectus rhoncus, iaculis neque in, feugiat dolor. Aenean finibus elit vel tellus egestas elementum. Maecenas porttitor, mauris eget tempor egestas, risus enim pretium purus, et suscipit lorem nunc a nisi. Ut hendrerit ligula sed ullamcorper elementum.
+
+Donec pulvinar nisl orci, et pellentesque ipsum porttitor sed. Nullam ipsum dolor, eleifend id purus id, efficitur eleifend nisl. Proin dapibus convallis nisi non interdum. Nam sit amet mattis velit. Phasellus blandit felis id enim suscipit, sit amet viverra est suscipit. Maecenas vehicula condimentum sem sit amet scelerisque. Proin scelerisque est tristique, vulputate lorem ut, molestie tellus.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec facilisis justo odio. Pellentesque gravida ornare nisi at tristique. Mauris erat arcu, vulputate ut tristique non, posuere feugiat felis. Curabitur mattis lacinia ante, at posuere purus rutrum ut. Cras in lectus rhoncus, iaculis neque in, feugiat dolor. Aenean finibus elit vel tellus egestas elementum. Maecenas porttitor, mauris eget tempor egestas, risus enim pretium purus, et suscipit lorem nunc a nisi. Ut hendrerit ligula sed ullamcorper elementum.
+
+Donec pulvinar nisl orci, et pellentesque ipsum porttitor sed. Nullam ipsum dolor, eleifend id purus id, efficitur eleifend nisl. Proin dapibus convallis nisi non interdum. Nam sit amet mattis velit. Phasellus blandit felis id enim suscipit, sit amet viverra est suscipit. Maecenas vehicula condimentum sem sit amet scelerisque. Proin scelerisque est tristique, vulputate lorem ut, molestie tellus.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec facilisis justo odio. Pellentesque gravida ornare nisi at tristique. Mauris erat arcu, vulputate ut tristique non, posuere feugiat felis. Curabitur mattis lacinia ante, at posuere purus rutrum ut. Cras in lectus rhoncus, iaculis neque in, feugiat dolor. Aenean finibus elit vel tellus egestas elementum. Maecenas porttitor, mauris eget tempor egestas, risus enim pretium purus, et suscipit lorem nunc a nisi. Ut hendrerit ligula sed ullamcorper elementum.
+
+Donec pulvinar nisl orci, et pellentesque ipsum porttitor sed. Nullam ipsum dolor, eleifend id purus id, efficitur eleifend nisl. Proin dapibus convallis nisi non interdum. Nam sit amet mattis velit. Phasellus blandit felis id enim suscipit, sit amet viverra est suscipit. Maecenas vehicula condimentum sem sit amet scelerisque. Proin scelerisque est tristique, vulputate lorem ut, molestie tellus.
             </p>
         </div>
     </div>
